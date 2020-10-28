f6e1948d9743c526ce4349ffed2ecd7653e6a584
tomas <Jamajka15351>
2020-10-28 14:10:23 +0200

    added 3 main routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home/home.component";
+import {RouletteComponent} from "./roulette/roulette.component";
+import {SessionComponent} from "./session/session.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'roulette',
+    component: RouletteComponent,
+  },
+  {
+    path: 'session',
+    component: SessionComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { SessionComponent } from './session/session.component';
+import { RouletteComponent } from './roulette/roulette.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SessionComponent,
+    RouletteComponent
   ],
   imports: [
     BrowserModule,
