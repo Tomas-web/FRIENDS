9116d922f63c3deb4ba665acc03e59c2d4730eb7
tomas <Jamajka15351>
2020-10-28 18:09:16 +0200

    created session page

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-session',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToHomePage() {
+    this.router.navigate(['home']);
   }
 
 }
